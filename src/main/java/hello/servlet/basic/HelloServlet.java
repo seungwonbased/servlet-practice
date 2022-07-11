40eade21c086585c13ac776fcaf888742b408fa8
baeseungwon <21storeo@gmail.com>
2022-07-11 21:59:06 +0900

    msg

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -13,8 +13,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-
-
     }
 
 
